5f40fb98dd669242d8e751869623203df6e15761
Abdullah Khawahir <abdullah.khawahir@gmail.com>
2024-12-23 13:36:58 +0300

    Added linkedin search open with a shortcut mod+o > l

---
+++ scripts/openLinked.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+firefox\
+"https://www.linkedin.com/jobs/search/?keywords=software%20engineer&geoId=100459316&distance=25&f_AL=true" \
+"https://www.linkedin.com/jobs/search/?keywords=software%20developer&geoId=100459316&distance=25&f_AL=true" \
+"https://www.linkedin.com/jobs/search/?keywords=full%20stack%20developer&geoId=100459316&distance=25&f_AL=true" \
+"https://www.linkedin.com/jobs/search/?keywords=backend%20developer&geoId=100459316&distance=25&f_AL=true" \
+"https://www.linkedin.com/jobs/search/?keywords=frontend%20developer&geoId=100459316&distance=25&f_AL=true" \
+"https://www.linkedin.com/jobs/search/?keywords=mobile%20app%20developer&geoId=100459316&distance=25&f_AL=true" \
+"https://www.linkedin.com/jobs/search/?keywords=devops%20engineer&geoId=100459316&distance=25&f_AL=true" \
+"https://www.linkedin.com/jobs/search/?keywords=cloud%20engineer&geoId=100459316&distance=25&f_AL=true" \
+"https://www.linkedin.com/jobs/search/?keywords=software%20architect&geoId=100459316&distance=25&f_AL=true" \
+"https://www.linkedin.com/jobs/search/?keywords=software%20engineer&geoId=100459316&distance=25&f_AL=true" \
+--new-window
+
